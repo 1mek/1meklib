f3f0d0d02319243d671cd39e5f3f801070117dab
1mek <47884814+1mek@users.noreply.github.com>
2020-10-09 10:30:28 +0200

    Update m8013.h

--- m8013.h
+++ m8013.h
@@ -1,7 +1,7 @@
 uint8_t m8013poll()
 {
     static uint32_t eee = 0;
-    static uint8_t _b,_s;
+    static uint8_t _s;
     
     if (millis() - eee >= 500UL) 
     {
@@ -9,7 +9,6 @@ uint8_t m8013poll()
       else { _s = 1; }
       
     }
-    _8013state = _s; 
     return _s;
 }
 uint8_t m8013(uint8_t a) { 
@@ -19,3 +18,26 @@ uint8_t m8013(uint8_t a) {
   else { return 0; } 
 }
 
+
+// In same.
+
+uint8_t m8013() {
+    static uint32_t eee=0; static uint8_t _s, _a, _b; out;
+    if (!_a && _b) { _b=0; out=0; }
+    else if (_a && !_b) { _b=1; out=1; }
+    else { out=0; }
+    
+    if (millis() - eee >= 500UL) { if (millis() - eee >= 1000UL) { _s = 0; eee=millis(); }  else { _s = 1; }      }
+    _a=_s;
+    return out;
+}
+uint8_t m8013(uint16_t _SWITCH, uint16_t _MAX) {
+    static uint32_t eee=0; static uint8_t _s, _a, _b; out;
+    if (!_a && _b) { _b=0; out=0; }
+    else if (_a && !_b) { _b=1; out=1; }
+    else { out=0; }
+    
+    if (millis() - eee >= _SWITCH) { if (millis() - eee >= _MAX) { _s = 0; eee=millis(); }  else { _s = 1; }      }
+    _a=_s;
+    return out;
+}
